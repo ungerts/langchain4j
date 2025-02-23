f18eedd482d62c09242fd340e77b05c2cef2e68d
Tobias Unger <ungerts@gmail.com>
2025-02-23 18:22:18 +0100

    Fix imports

--- langchain4j-cassandra/src/test/java/dev/langchain4j/store/memory/chat/cassandra/CassandraChatMemoryStoreTestSupport.java
+++ langchain4j-cassandra/src/test/java/dev/langchain4j/store/memory/chat/cassandra/CassandraChatMemoryStoreTestSupport.java
@@ -4,8 +4,6 @@ import dev.langchain4j.data.message.AiMessage;
 import dev.langchain4j.data.message.UserMessage;
 import dev.langchain4j.memory.ChatMemory;
 import dev.langchain4j.memory.chat.MessageWindowChatMemory;
-import dev.langchain4j.memory.chat.TokenWindowChatMemory;
-import dev.langchain4j.model.openai.OpenAiTokenizer;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.MethodOrderer;
@@ -18,13 +16,12 @@ import java.util.UUID;
 
 import static dev.langchain4j.data.message.AiMessage.aiMessage;
 import static dev.langchain4j.data.message.UserMessage.userMessage;
-import static dev.langchain4j.model.openai.OpenAiModelName.GPT_3_5_TURBO;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 @Slf4j
 abstract class CassandraChatMemoryStoreTestSupport {
-    protected final String KEYSPACE = "langchain4j";
+    protected static final String KEYSPACE = "langchain4j";
     protected static CassandraChatMemoryStore chatMemoryStore;
 
     @Test
